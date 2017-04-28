fef9bd928211a0d93635f91889283b363ebbcddb
luciacr1 <luciacr9925@gmail.com>
2017-04-28 14:00:47 -0300

    Baseado no ultimo commit da manu, estou enviando novamente pq ele falou que nao comitei minhas mudancas apesar de n ter feito nenhuma

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+# Para criar texto
+
+#def mensagem_tela (msg, cor, y_displace=0, size = 'small'):
+	#smallfont = fpygame.font.SysFont('comicsamsms', 25)
+	#medfont = fpygame.font.SysFont('comicsamsms', 50)
+	#largefont = fpygame.font.SysFont('comicsamsms', 80)
+	#texto_tela = font.render(msg, True, cor)
+	#gameDisplay.blit(texto_tela, [display_widht/2, display_height/2])
+
+	
+	#caso o jogador perca
+
+	#mensagem_tela('Loser :/', color='red')
+	#pygame.display.update()
+	#time.sleep(2)
 import pygame , sys
 from pygame.locals import *
 class monstros:
@@ -7,15 +22,22 @@ class monstros:
 		self.topo = tela.get_height() - self.imagem.get_height()
 		self.centro = tela.get_width()/2 - self.imagem.get_width()/2
 
-	def desenha(self):
+def desenha(self):
 		self.tela.blit(self.imagem,(self.centro,self.topo))
 
 
 
+# Jogo Principal
+
+import time
+import pygame , sys
+from pygame.locals import *
 
 clock = pygame.time.Clock()
 tela = pygame.display.set_mode((800,600))
 pygame.mouse.set_visible(0)
+
+nave = pygame.image.load("ship.png")
 
 nave = pygame.image.load("nave_pequena.png")
 nave_topo = tela.get_height() - nave.get_height()
@@ -26,6 +48,7 @@ pygame.display.set_caption("Space invaders - Code Girls")
 
 monstro = monstros(tela,"monstrinho.png")
 
+tela.blit(nave, (nave_esq,nave_topo))
 #monstro = pygame.image.load("monstrinho.png")
 
 tela.blit(nave, (nave_esq,nave_topo), )
@@ -44,16 +67,4 @@ while True:
 	monstro.desenha()
 	
 	
-
 	for evento in pygame.event.get():
-		if evento.type == pygame.QUIT:
-			sys.exit()
-		elif evento.type == MOUSEBUTTONDOWN:
-			atirar_y = nave_topo
-			atirar_x = x
-
-	if atirar_y > 0:
-		tela.blit(tiro, (atirar_x,atirar_y))
-		atirar_y -= 10
-
-	pygame.display.update()
