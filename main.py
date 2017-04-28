f89785b0f3981021320807ca8dfff1bacbedab30
manucirne <manucirne@gmail.com>
2017-04-28 09:18:31 -0300

    agora sim, montrinho em classe

--- main.py
+++ main.py
@@ -1,44 +1,39 @@
-# Para criar texto
-
-#def mensagem_tela (msg, cor, y_displace=0, size = 'small'):
-	#smallfont = fpygame.font.SysFont('comicsamsms', 25)
-	#medfont = fpygame.font.SysFont('comicsamsms', 50)
-	#largefont = fpygame.font.SysFont('comicsamsms', 80)
-	#texto_tela = font.render(msg, True, cor)
-	#gameDisplay.blit(texto_tela, [display_widht/2, display_height/2])
-
-	
-	#caso o jogador perca
-
-	#mensagem_tela('Loser :/', color='red')
-	#pygame.display.update()
-	#time.sleep(2)
-
-
-
-
-# Jogo Principal
-
-import time
 import pygame , sys
 from pygame.locals import *
+class monstros:
+	def __init__(self,tela,imagem):
+		self.imagem = pygame.image.load(imagem)
+		self.tela = tela
+		self.topo = tela.get_height() - self.imagem.get_height()
+		self.centro = tela.get_width()/2 - self.imagem.get_width()/2
+
+	def desenha(self):
+		self.tela.blit(self.imagem,(self.centro,self.topo))
+
+
+
 
 clock = pygame.time.Clock()
 tela = pygame.display.set_mode((800,600))
 pygame.mouse.set_visible(0)
 
-nave = pygame.image.load("ship.png")
+nave = pygame.image.load("nave_pequena.png")
 nave_topo = tela.get_height() - nave.get_height()
 nave_esq = tela.get_width()/2 - nave.get_width()/2
 pygame.display.set_caption("Space invaders - Code Girls")
+#monstro_topo = tela.get_height() - monstro.get_height()
+#monstro_centro = tela.get_width()/2 - monstro.get_width()/2
 
-tela.blit(nave, (nave_esq,nave_topo))
+monstro = monstros(tela,"monstrinho.png")
+
+#monstro = pygame.image.load("monstrinho.png")
+
+tela.blit(nave, (nave_esq,nave_topo), )
 
 background = pygame.image.load("espaco.jpg")
 
 tiro = pygame.image.load("shot.png")
 atirar_y = 0
-
 
 while True:
 	clock.tick(60)
@@ -46,6 +41,8 @@ while True:
 	#tela.fill((0,0,0))
 	x,y = pygame.mouse.get_pos()
 	tela.blit(nave, (x-nave.get_width()/2,nave_topo))
+	monstro.desenha()
+	
 	
 
 	for evento in pygame.event.get():
