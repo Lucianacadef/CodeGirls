7b2cbb3d03ea8b08ed0286a4e91e5a70b8787d55
manucirne <manucirne@gmail.com>
2017-05-11 13:48:35 -0300

    Arrumamos a nave mae e os monstrinhos estão se movimentando de duas formas diferentes - gif e para os lados

--- testescodegirls.py
+++ testescodegirls.py
@@ -9,18 +9,11 @@ pygame.init()
 pygame.mixer.init()
 pygame.font.init()
 
-# def interseccao(s1_x, s1_y, s2_x, s2_y):
-# 	return (s1_x > s2_x - 10) and \
-# 	       (s1_y > s2_y - 10) and \
-# 	       (s1_x < s2_x + 10) and \
-# 	       (s1_y < s2_y + 10)
-
 
 class jogador(pygame.sprite.Sprite):
 	def __init__(self,tela,imagem):
 		pygame.sprite.Sprite.__init__(self)
 		self.image = pygame.image.load(imagem)
-	#self.image = pygame.transform.scale(image, (50,50)) #para dimensionar mas nao deu certp
 		self.tela = tela
 
 
@@ -36,14 +29,16 @@ class jogador(pygame.sprite.Sprite):
 		self.tela.blit(self.image,(nave_topo,
 			                        nave_esq))
 
-
-class monstros(pygame.sprite.Sprite):
-	def __init__(self,tela,imagem):
+class monstrosgif(pygame.sprite.Sprite):
+	def __init__(self,tela,imagem1,imagem2):
 		pygame.sprite.Sprite.__init__(self)
 
-		self.image = pygame.image.load(imagem)
+		self.image = pygame.image.load(imagem1)
+		self.image1 = pygame.image.load(imagem1)
+		self.image2 = pygame.image.load(imagem2)
 		self.tela = tela
-		self.passo = 0
+		self.passo1 = 0
+		self.passo2 = 0
 		self.direcao = 1
 		self.type = 1
 
@@ -58,12 +53,22 @@ class monstros(pygame.sprite.Sprite):
 
 
 	def update(self):
-		if self.passo == 10:
+		if self.passo1 < 15:
+			self.image = self.image2
+		elif self.passo1 >= 15:
+			self.image = self.image1
+		if self.passo1 == 30:
+			self.passo1 = 0
+		else:
+			self.passo1 += 1
+
+		if self.passo2 == 10:
 			self.direcao = -1
-		elif self.passo == -10:
+		elif self.passo2 == -10:
 			self.direcao = 1
-		self.passo += self.direcao
+		self.passo2 += self.direcao
 		self.rect.x += self.direcao
+
 ################### nave mae ############################
 class navemae(pygame.sprite.Sprite):
 	def __init__(self,tela,imagem):
@@ -76,7 +81,7 @@ class navemae(pygame.sprite.Sprite):
 		self.type = 1
 
 	def posicao(self,x,y):
-		self.rect = self.image.get_rect() #ta da
+		self.rect = self.image.get_rect() 
 		self.rect.x = x
 		self.rect.y = y
 
@@ -86,10 +91,15 @@ class navemae(pygame.sprite.Sprite):
 
 
 	def update(self):
-		if self.rect.x < 800:
-			self.rect.x += 3
-		elif self.rect.x >= 800:
-			self.rect.x -= 3
+		if self.passo >= 715:
+			self.direcao = -3
+		elif self.passo <= 0:
+			self.direcao = 3
+	
+		
+		self.passo += self.direcao
+		self.rect.x += self.direcao
+
 
 ########################################################
 class shots(pygame.sprite.Sprite):
@@ -147,26 +157,6 @@ myfont = pygame.font.SysFont('Arial', 30)
 myfont1 = pygame.font.SysFont('Arial', 100)
 fim = myfont1.render('Você venceu!!!!!!', False, (255,255,255))
 
-#TENTANDO COLOCAR O GIF AO INVES DA IMG INICIAL
-m1 = pygame.image.load("m1.png")
-m4 = pygame.image.load("m3.png")
-
-#FALTA A OUTRAS MUSICAS, ESTA DANDO PAU  
-
-# lista_monstros = []
-# for i in range(400,560,40):
-# 	for j in range(-300,300,40):
-# 		novo_monstro = monstros(tela,"monstrinho.png", j, -i)
-# 		lista_monstros.append(novo_monstro)
-
-# tiro = shots(tela, "shot.png")
-# tiro2 = shots(tela, "shot.png")
-# tiro3 = shots(tela, "shot.png")
-# tiro4 = shots(tela, "shot.png")
-# tiro5 = shots(tela, "shot.png")
-#atirar_y = 0
-#atirar_x = 0
-#tiros = []
 
 grupo_tiros = pygame.sprite.Group()
 tiro = 0
@@ -175,11 +165,10 @@ pontos = 0
 
 
 grupo_monstro = pygame.sprite.Group()
-for i in range(170,210,40):
-	for j in range(300,400,40):
-		#novo_monstro = monstros(tela,"monstrinho.png", j, -i)
-		#novo_monstro.desenha()
-		novo_monstro = monstros(tela,"monstrinho.png")
+for i in range(100,300,40):
+	for j in range(100,700,40):
+
+		novo_monstro = monstrosgif(tela,"m1.png","m3.png")	
 		novo_monstro.posicao(j, i)
 		grupo_monstro.add(novo_monstro)
 
@@ -194,7 +183,8 @@ for i in range(170,210,40):
 m1 = pygame.image.load("m1.png")
 m2 = pygame.image.load("m3.png")
 monstrosCurrentImage = 1
-
+contagem = 0
+acertosnavemae = 0
 
 ####################################################################################
 
@@ -211,29 +201,6 @@ while True:
 			shoot_sound.play()
 			grupo_tiros.draw(tela)
 
-			# for b in range(len(tiros)):
-			# 	tiros[b][0]-=10
-
-			# for t in tiros:
-			# 	if t[0]<0:
-			# 		tiros.remove(t)
-
-
-			#atirar_y = nave_topo
-			#atirar_x = x
-	
-	if (monstrosCurrentImage==1): 
-		tela.blit(m1, (100,100)) 
-
-	if (monstrosCurrentImage==2): 
-		tela.blit(m2, (100,100))
-
-	if (monstrosCurrentImage==2): 
-		monstrosCurrentImage=1
-
-	else: 
-		monstrosCurrentImage+=1 
-	pygame.display.flip() 
 	
 
 #---------------------------------#
@@ -248,44 +215,28 @@ while True:
 	grupo_nave_mae.draw(tela)
 	grupo_monstro.draw(tela)
 	grupo_tiros.draw(tela)
-					
 
-	#if atirar_y > 0:
-		#tela.blit(tiro, (atirar_x,atirar_y))
-		#atirar_y -= 15
 
-#aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+
 
 	col = pygame.sprite.groupcollide(grupo_tiros,grupo_monstro,True,True)
+	colnavemae = pygame.sprite.groupcollide(grupo_tiros,grupo_nave_mae,True,False)
 	for i in col:
 		pontos += 1
 	ponto = myfont.render('Pontuação: {0}'.format(pontos), False, (255,255,255))
 	tela.blit(ponto,(10,10))
 
-	colnavemae = pygame.sprite.groupcollide(grupo_tiros,grupo_nave_mae,True,True)
+	for i in colnavemae:
+		acertosnavemae += 1	
+	if acertosnavemae >= 10:
+		nave_mae.kill()
+		pontos += 10
+		acertosnavemae = 0
 
-	#print(pontos)
-
-	# for monstro in monstros_atingidos:
-	# 	grupo_monstro.kill(novo_monstro)
-
-
-	# lista_sobreviventes = []
-	# for k in range(len(grupo_monstro)):
-	# 	monstro = grupo_monstro[k]
-	# 	if not interseccao(monstro.rect.x, monstro.rect.y, atirar_x, atirar_y):
-	# 		lista_sobreviventes.append(k)
-
-	# monstros_restantes = []
-	# for k in lista_sobreviventes:
-	# 	monstros_restantes.append(lista_monstros[k])
-
-	# lista_monstros = monstros_restantes
 	if len(grupo_monstro) == 0 and len(grupo_nave_mae) == 0:
 		tela.fill([0,0,0])
 		tela.blit(fim,(100,220))
 
-#Fazendo 
 	grupo_tiros.update()
 	grupo_monstro.update()
 	grupo_nave_mae.update()
